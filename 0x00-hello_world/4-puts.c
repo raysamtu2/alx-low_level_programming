bb78f10f50bc186857b07693f77bc4c009236c27
raysamtu2 <raysamtu2@gmail.com>
2022-09-12 18:26:24 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+* main - print the string in the put functions
+
+*
+
+* Description: using the main functions
+
+* this program prints "Programming is like building a blocks
+
+* Return: 0
+
+*/
+
+int main(void)
+{
+
+	puts("\"Programming is like building a blocks");
+
+        return (0);
+
+}
