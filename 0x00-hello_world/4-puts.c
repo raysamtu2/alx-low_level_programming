e4d9597e6d4928005a6ec86de2837b3e02da4b04
raysamtu2 <raysamtu2@gmail.com>
2022-09-12 18:37:31 -0700

    cooding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,22 +2,13 @@
 
 /**
 * main - print the string in the put functions
-
 *
-
 * Description: using the main functions
-
-* this program prints "Programming is like building a blocks
-
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
-
 */
-
 int main(void)
 {
-
-	puts("\"Programming is like building a blocks");
-
+	puts("\"Programming is like building a multilingual puzzle");
         return (0);
-
 }
