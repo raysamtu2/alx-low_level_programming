b589b485c984f689476909e0ce4bd851160923cb
raysamtu2 <raysamtu2@gmail.com>
2022-09-13 19:05:17 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _samuelislower - function to check for lowercase character
+ * @c:  i know tha at the end of thr day int that will use for the argument of the function
+ * Return: 0
+ */
+int _samuelislower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
