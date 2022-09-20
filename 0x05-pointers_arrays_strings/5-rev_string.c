12cd35e15c02b3eb54dd12016b7a901a1ce9ed3d
raysamtu2 <raysamtu2@gmail.com>
2022-09-19 17:23:18 -0700

    all

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,13 +10,13 @@
 
 void rev_string(char *s)
 {
-	int i, tmp, sam = _strlen(s);
+	int i, tmp, len = _strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = *(s + i);
-		*(s + i) = *(s + sam - i - 1);
-		*(s + sam - i - 1) = tmp;
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
 	}
 }
 
@@ -29,10 +29,10 @@ void rev_string(char *s)
 
 int _strlen(char *s)
 {
-	int sam = 0;
+	int len = 0;
 
-	while (*(s + sam) != '\0')
-		sam++;
+	while (*(s + len) != '\0')
+		len++;
 
-	return (sam);
+	return (len);
 }
