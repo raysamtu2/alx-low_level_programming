96be54fd3579b82b2e0ce4756d80be4f9e3485b6
raysamtu2 <raysamtu2@gmail.com>
2022-09-27 08:19:43 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,36 @@
+
+
+es n bytes from source memory to destination
+
+ *
+ *
+ *  * @dest: destination to write to
+ *
+ *   * @src: source to copy from
+ *
+ *    * @n: number of bytes to write
+ *
+ *     *
+ *
+ *      * Return: pointer to dest
+ *
+ *       */
+
+ char *_memcpy(char *dest, char *src, int n)
+
+{
+
+		char *ptr = dest;
+
+
+
+			while (n--)
+
+						*ptr++ = *src++;
+
+
+
+				return (dest);
+
+}
+#include <stdio.h>
